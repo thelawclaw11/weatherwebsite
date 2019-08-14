74da1d76cbcd77abb2ee2c9162c10fbce7601c1a
thelawclaw11 <canon@lawcasa.com>
2019-08-14 04:09:03 -0600

    =try4

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Canon Law'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Canon Law'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Canon Law'
     })
 })
 
